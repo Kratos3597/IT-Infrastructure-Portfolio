7bdab43cbdf6d93a2e354c3bce2cdb4cfee8892b
Mohammed Sheik <128423704+Kratos3597@users.noreply.github.com>
2026-05-26 16:17:46 +0200

    Update sat.bat

--- System-Admin-Toolkit/sat.bat
+++ System-Admin-Toolkit/sat.bat
@@ -1,2 +1,12 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0menu.ps1"
+$repo = "https://github.com/Kratos3597/System-Admin-Toolkit"
+$local = "$env:TEMP\SAT"
+
+if (!(Test-Path $local)) {
+    git clone $repo $local
+}
+else {
+    cd $local
+    git pull
+}
+
+powershell -ExecutionPolicy Bypass -File "$local\menu.ps1"
