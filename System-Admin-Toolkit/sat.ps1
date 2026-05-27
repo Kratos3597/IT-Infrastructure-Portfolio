7c7dce7b19c00437b939825e23654516cbb9241c
Mohammed Sheik <128423704+Kratos3597@users.noreply.github.com>
2026-05-27 07:58:09 +0200

    Update sat.ps1

--- System-Admin-Toolkit/sat.ps1
+++ System-Admin-Toolkit/sat.ps1
@@ -1,12 +1,14 @@
-$repo = "https://github.com/Kratos3597/System-Admin-Toolkit"
+$base = "https://raw.githubusercontent.com/Kratos3597/IT-Infrastructure-Portfolio/main/System-Admin-Toolkit"
 $local = "$env:TEMP\SAT"
 
+# Create folder
 if (!(Test-Path $local)) {
-    git clone $repo $local
-}
-else {
-    cd $local
-    git pull
+    New-Item -ItemType Directory -Path $local | Out-Null
 }
 
+# Download required files
+Invoke-RestMethod "$base/menu.ps1" -OutFile "$local\menu.ps1"
+Invoke-RestMethod "$base/system.ps1" -OutFile "$local\system.ps1"
+
+# Run menu
 powershell -ExecutionPolicy Bypass -File "$local\menu.ps1"
